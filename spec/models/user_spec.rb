348e7cf1a366c6a8d4fb8dadec1c4800d1fd0c1c
Jaime Rump <jaime.lee.rump@gmail.com>
2017-12-14 07:54:16 -0500

    Try Devise for registration/login

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  name                   :string
+#  screen_name            :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
